e07ff01a2123521b8b967796642f7730d19c8995
T14g <ottiagos@gmail.com>
2025-04-27 22:11:11 -0300

    added material snackbar

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../product.service';
 import { Product } from 'src/app/models/product';
+import { CartService } from 'src/app/cart/cart.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit{
+export class ProductListComponent implements OnInit {
 
-  products : Product[] = [];
+  products: Product[] = [];
 
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    private cartService: CartService,
+    private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe(data => {
@@ -19,4 +24,18 @@ export class ProductListComponent implements OnInit{
     })
   }
 
+  addToCart(product: Product): void {
+    // addToCart is a observable so subscribe to it!
+    this.cartService.addToCart(product).subscribe(
+      {
+        next: () => {
+          this.snackBar.open('Product added to cart!', "", {
+            duration: 2000,
+            horizontalPosition: 'right',
+            verticalPosition: 'top'
+          })
+        }
+      }
+    );
+  };
 }
--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import { MatCardModule } from '@angular/material/card'
 import { FlexModule } from '@angular/flex-layout';
-
+import { MatSnackBarModule } from '@angular/material/snack-bar'
 @NgModule({
   declarations: [
     ProductListComponent
@@ -11,7 +11,7 @@ import { FlexModule } from '@angular/flex-layout';
   imports: [
     CommonModule,
     MatCardModule,
-    FlexModule
+    FlexModule, MatSnackBarModule
   ]
 })
 export class ProductModule { }
